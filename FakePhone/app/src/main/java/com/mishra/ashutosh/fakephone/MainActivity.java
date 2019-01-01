927e061029c0290a0279d019077ac2cec109b590
Ashu-172 <mishraashutosh801@gmail.com>
2019-01-01 17:41:00 +0530

    Fourth App initial Commit

---
+++ FakePhone/app/src/main/java/com/mishra/ashutosh/fakephone/MainActivity.java
@@ -0,0 +1,23 @@
+package com.mishra.ashutosh.fakephone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvMessage;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvMessage = findViewById(R.id.tvMessage);
+        if(getIntent().getData()!=null){
+            tvMessage.setText(getIntent().getData().toString());
+        }
+        else{
+            tvMessage.setText("NoData Recieved..!");
+        }
+    }
+}
